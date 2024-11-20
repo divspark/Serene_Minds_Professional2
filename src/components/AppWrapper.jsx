7ad4ef504d239f4625eca3bbf7b4765cec10d909
divspark <divyansh2004mhj@gmail.com>
2024-11-20 15:23:47 +0530

    Fine Changes

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -5,13 +5,22 @@ import AdBanner from "./Ad/AdBanner";
 export default function AppWrapper() {
   return (
     <main className="flex bg-white h-screen">
-      <aside className="w-[290px] h-ful sticky top-0">
+      {/* Fixed Sidebar */}
+      <aside className="w-[290px] fixed top-0 left-0 h-full z-10">
         <Slidercomp />
       </aside>
-      <div className="flex flex-1 flex-col h-screen">
-        <Header />
-        {/* <AdBanner /> */}
-        <Outlet />
+
+      {/* Main Content Area */}
+      <div className="flex flex-1 flex-col ml-[290px] h-screen">
+        {/* Sticky Header */}
+        <header className="sticky top-0 ">
+          <Header />
+        </header>
+
+        {/* Main Content with Scroll */}
+        <section className="flex-1 overflow-y-auto p-4">
+          <Outlet />
+        </section>
       </div>
     </main>
   );
