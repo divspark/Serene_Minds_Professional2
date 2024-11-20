edf4687c02f789ece4d2b154d7ee1f2164286ab6
divspark <divyansh2004mhj@gmail.com>
2024-11-20 14:16:14 +0530

    New Changes

--- src/components/Ad/AdBanner.jsx
+++ src/components/Ad/AdBanner.jsx
@@ -1,7 +1,7 @@
 import Poster from "../../assets/poster.png";
 export default function AdBanner() {
   return (
-    <div className="p-5">
+    <div className="px-1 py-3">
       <img src={Poster} alt="Poster" className="w-full object-cover" />
     </div>
   );
