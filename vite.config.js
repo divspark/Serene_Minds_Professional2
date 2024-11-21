d49d26e1f3136d8831f8fb3a0e6844ed39cb387b
divspark <divyansh2004mhj@gmail.com>
2024-11-21 14:29:48 +0530

    Update route

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // Set the base path for all assets
   base: '/',
+  
+  build: {
+    outDir: 'dist', // The output directory for the build
+    rollupOptions: {
+      input: 'index.html', // Ensures index.html is the entry point
+    }
+  }
 })
