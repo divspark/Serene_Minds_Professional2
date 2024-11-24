cbf0670a7cb8b3e74f205af1d680d09fcde0f739
divspark <divyansh2004mhj@gmail.com>
2024-11-24 13:30:56 +0530

    added .env base url

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -12,6 +12,8 @@ import {
 } from "chart.js";
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 // Register the required components
 ChartJS.register(
   BarController,
